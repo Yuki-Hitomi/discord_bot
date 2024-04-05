086373c52b827da66b6e8ab44192d5d9d6ddaf19
Yuki-Hitomi <165801997+Yuki-Hitomi@users.noreply.github.com>
2024-04-05 21:51:51 +0900

    Update diceroll.js

--- commands/utility/diceroll.js
+++ commands/utility/diceroll.js
@@ -4,24 +4,30 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('diceroll')
 		.setDescription('Roll the dice!')
-		.addIntegerOption(option =>
+		.addStringOption(option =>
 			option.setName('dice')
 				.setDescription('ダイスの個数')
-				.setRequired(true))
-		.addIntegerOption(option =>
+				.setRequired(true)
+                .addChoice('1', '1')
+                .addChoice('2', '2')
+                .addChoice('3', '3'))
+		.addStringOption(option =>
 			option.setName('sides')
 				.setDescription('ダイスの面数')
-				.setRequired(true)),
+				.setRequired(true)
+                .addChoice('6', '6')
+                .addChoice('8', '8')
+                .addChoice('10', '10')),
 
 	async execute(interaction) {
-		const dice = interaction.options.getInteger('dice');
-		const sides = interaction.options.getInteger('sides');
+		const dice = interaction.options.getString('dice');
+		const sides = interaction.options.getString('sides');
 		let results = [];
 		let total = 0;
 
 		for(let i = 0; i < dice; i++) {
 			let roll = Math.floor(Math.random() * sides) + 1;
-			results.push(`ダイス${(i+1).toString().padStart(3, '0')}: ${roll}`);
+			results.push(`ダイス${(i+1).toString().padStart(2, '0')}: ${roll}`);
 			total += roll;
 		}
 
