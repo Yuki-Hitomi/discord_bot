8fb4d3cda53cb77830111a9977444c9b607afc98
Yuki-Hitomi <165801997+Yuki-Hitomi@users.noreply.github.com>
2024-04-05 21:58:58 +0900

    Update diceroll.js

--- commands/utility/diceroll.js
+++ commands/utility/diceroll.js
@@ -7,21 +7,29 @@ module.exports = {
 		.addIntegerOption(option =>
 			option.setName('dice')
 				.setDescription('ダイスの個数')
-				.setRequired(true))
-		.addIntegerOption(option =>
+				.setRequired(true)
+				.addNumberValidator(value => value >= 1 && value <= 99)) // ダイスの個数の範囲を1から99に制限
+		.addStringOption(option =>
 			option.setName('sides')
 				.setDescription('ダイスの面数')
-				.setRequired(true)),
+				.setRequired(true)
+				.addChoice('4', 4)
+				.addChoice('6', 6)
+				.addChoice('8', 8)
+				.addChoice('10', 10)
+				.addChoice('12', 12)
+				.addChoice('20', 20)
+				.addChoice('100', 100)), // ダイスの面数の選択肢を指定
 
 	async execute(interaction) {
 		const dice = interaction.options.getInteger('dice');
-		const sides = interaction.options.getInteger('sides');
+		const sides = interaction.options.getString('sides');
 		let results = [];
 		let total = 0;
 
-		for(let i = 0; i < dice; i++) {
-			let roll = Math.floor(Math.random() * sides) + 1;
-			results.push(`ダイス${(i+1).toString().padStart(2, '0')}: ${roll}`);
+		for (let i = 0; i < dice; i++) {
+			let roll = Math.floor(Math.random() * parseInt(sides)) + 1;
+			results.push(`ダイス${(i + 1).toString().padStart(2, '0')}: ${roll}`);
 			total += roll;
 		}
 
