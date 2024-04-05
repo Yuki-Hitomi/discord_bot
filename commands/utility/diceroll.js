4e4f912b9efc43c8a683e4bb8fa94e2bc30d5144
Yuki-Hitomi <165801997+Yuki-Hitomi@users.noreply.github.com>
2024-04-05 21:11:55 +0900

    Create diceroll.js

---
+++ commands/utility/diceroll.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('roll')
+		.setDescription('Roll the dice!')
+		.addIntegerOption(option =>
+			option.setName('dice')
+				.setDescription('ダイスの個数')
+				.setRequired(true))
+		.addIntegerOption(option =>
+			option.setName('sides')
+				.setDescription('ダイスの面数')
+				.setRequired(true)),
+
+	async execute(interaction) {
+		const dice = interaction.options.getInteger('dice');
+		const sides = interaction.options.getInteger('sides');
+		let results = [];
+		let total = 0;
+
+		for(let i = 0; i < dice; i++) {
+			let roll = Math.floor(Math.random() * sides) + 1;
+			results.push(roll);
+			total += roll;
+		}
+
+		await interaction.reply(`ダイスを振った結果は ${total} です！それぞれのダイスの結果は次のとおりです：${results.join(', ')}`);
+	},
+};
