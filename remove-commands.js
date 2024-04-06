04ffe1b1c392047feb448cd07858ffea862d5e33
Yuki-Hitomi <165801997+Yuki-Hitomi@users.noreply.github.com>
2024-04-07 00:23:49 +0900

    Update remove-commands.js

--- remove-commands.js
+++ remove-commands.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
-const fs = require('fs');
-const path = require('path');
+const { clientId, guildId, token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const commands = [];
 
