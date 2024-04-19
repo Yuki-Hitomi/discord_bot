3c58bf581960ccf734584c114e49a6bdabf211d8
Yuki-Hitomi <165801997+Yuki-Hitomi@users.noreply.github.com>
2024-04-20 00:26:54 +0900

    カラーコードの表示を修正

--- commands/production/color.js
+++ commands/production/color.js
@@ -8,20 +8,20 @@ module.exports = {
     async execute(interaction) {
         const randomColor = getRandomColor();
         
-        await interaction.reply({ content: `${interaction.member.displayName}のラッキーカラーは${randomColor}です！`, embeds: [createColorEmbed(randomColor)] });
+        await interaction.reply({ content: `${interaction.member.displayName}のラッキーカラーは#${randomColor.toUpperCase()}です！`, embeds: [createColorEmbed(randomColor)] });
         
         logger.log(`/color was executed by ${interaction.user.username}`);
     },
 };
 
 function getRandomColor() {
-    const color = '0x' + Math.floor(Math.random()*16777215).toString(16);
+    const color = Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
     return color;
 }
 
 function createColorEmbed(color) {
     return {
-        color: Number(color),
-        description: color,
+        color: parseInt(color, 16),
+        description: `#${color.toUpperCase()}`,
     };
 }
