bf4a73a4bb8b28290791fcf3cf2e2df9162de50b
Yuki-Hitomi <165801997+Yuki-Hitomi@users.noreply.github.com>
2024-04-26 23:34:53 +0900

    運勢テーブルのsqlを更新、色テーブルを追加

--- sql/create_fortuns.sql
+++ sql/create_fortuns.sql
@@ -3,15 +3,16 @@ CREATE TABLE fortunes (
     fortune_text TEXT
 );
 
-INSERT INTO fortunes (fortune_text) VALUES ('大吉');
-INSERT INTO fortunes (fortune_text) VALUES ('中吉');
-INSERT INTO fortunes (fortune_text) VALUES ('小吉');
-INSERT INTO fortunes (fortune_text) VALUES ('吉');
-INSERT INTO fortunes (fortune_text) VALUES ('半吉');
-INSERT INTO fortunes (fortune_text) VALUES ('末吉');
-INSERT INTO fortunes (fortune_text) VALUES ('末小吉');
-INSERT INTO fortunes (fortune_text) VALUES ('凶');
-INSERT INTO fortunes (fortune_text) VALUES ('小凶');
-INSERT INTO fortunes (fortune_text) VALUES ('半凶');
-INSERT INTO fortunes (fortune_text) VALUES ('末凶');
-INSERT INTO fortunes (fortune_text) VALUES ('大凶');
+INSERT INTO fortunes (fortune_text) VALUES
+('大吉'),
+('中吉'),
+('小吉'),
+('吉'),
+('半吉'),
+('末吉'),
+('末小吉'),
+('凶'),
+('小凶'),
+('半凶'),
+('末凶'),
+('大凶');
