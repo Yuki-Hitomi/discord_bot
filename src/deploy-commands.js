e52d3653060861d9cdf6a6aa340d2e29dfbd85ce
Yuki-Hitomi <165801997+Yuki-Hitomi@users.noreply.github.com>
2024-11-28 23:08:43 +0900

    デプロイ中のログ追加

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -8,6 +8,7 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
+	console.info(`Deploying ${folder} commands...`);
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
